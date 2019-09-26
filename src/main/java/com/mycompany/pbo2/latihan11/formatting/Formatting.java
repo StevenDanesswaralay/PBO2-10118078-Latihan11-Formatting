6a01ca76d57a5c192a7cc593c66620562eaa8b21
StevenDanesswaralay <55594713+StevenDanesswaralay@users.noreply.github.com>
2019-09-26 11:23:20 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/pbo2/latihan11/formatting/Formatting.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbo2.latihan11.formatting;
+
+/**
+ *
+ * @author 
+ * NAMA     : Steven Danesswaralay
+ * KELAS    : PBO2
+ * NIM      : 10118078
+ * Deskripsi Program : Formatting
+ */
+public class Formatting {
+    public static void main(String[] args) {
+        int i = 3546764;
+        int iMinus = -i;
+        System.out.println("i : " + i);
+        System.out.printf("%%d : %d%n", i);
+        System.out.printf("%%10d : %10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", iMinus);
+        System.out.printf("%%,10d : %,10d$n", i);
+        System.out.printf("%%-10d : %-10d%n", i);
+        double f = 5675482.982;
+        System.out.println("f : " + f);
+        System.out.printf("%%f : %f%n", f);
+        System.out.printf("%%.2f : %2f%n", f);
+        System.out.printf("%%12.2f : %12.2f%n", f);
+        System.out.printf("%%,12.2f : %,12.2f%n", f);
+    }
+}
